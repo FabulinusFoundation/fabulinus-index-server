9f9d163e31db1d4dc222fc10b0c402357d74db95
Timon Langlotz <timon.langlotz@gmail.com>
2015-05-09 21:53:34 +0200

    added i18n

--- server.js
+++ server.js
@@ -2,23 +2,63 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var morgan  = require('morgan');
 var path    = require('path');
+var fs      = require('fs');
 
-var kaliteUrl = "http://localhost:8008";
-var wikiUrl   = "http://localhost/mediawiki";
-var port      = process.env.NODE_PORT || 3000;
+var port      = process.env.NODE_PORT || 80;
 
 var app = express();
 
 app.use(morgan('dev'));
-console.log(path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'logo.ico')));
-
 app.set('view engine', 'ejs');
 
+
 app.get('/', function (req, res) {
-	res.render('index.ejs', { kaliteUrl : kaliteUrl, wikiUrl : wikiUrl });
+    getI18N(function (err, languages) {
+        if (err){
+            console.log(err);
+        }
+        var custom = 'en';
+        var selected = req.query.lang || custom;
+        var customStrings;
+        var selectedStrings;
+        for (var i=0; i<languages.length; i++){
+            if (languages[i].id == selected){
+                selectedStrings = languages[i].strings;
+            }
+            if (languages[i].id == custom){
+                customStrings = languages[i].strings;
+            }
+        }
+        res.render('index.ejs', { languages : languages, strings : selectedStrings || customStrings });
+    });
 });
+
+
+function getI18N(done){
+    var dir = path.join(__dirname, 'public', 'i18n');
+    var languages = [];
+
+    fs.readdir(dir,function(err, files){
+        if (err){
+            return done(err, languages);
+        }
+        var count = 0;
+        files.forEach(function(file){
+            fs.readFile(path.join(dir, file),'utf-8',function(err, language){
+                if (err){
+                    return done(err, languages);
+                }
+                var json = JSON.parse(language);
+                languages.push(json);
+                if (++count == files.length){
+                    return done(null, languages);
+                }
+            });
+        });
+    });
+}
 
 
 var server = app.listen(port, function () {
