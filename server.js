b97d55796d0b19f82f07a949dfd32540d44a9d23
Timon Langlotz <timon.langlotz@gmail.com>
2015-07-05 18:45:40 +0200

    test

--- server.js
+++ server.js
@@ -3,11 +3,33 @@ var favicon = require('connect-favicons');
 var morgan  = require('morgan');
 var path    = require('path');
 var fs      = require('fs');
+var os 		= require('os');
 
-var host      = '1.1.1.1';
-var port      = process.env.NODE_PORT || 3000;
-var kaliteUrl = 'http://' + host + ':8008';
-var wikiUrl   = 'http://' + host + ':8080';
+var host, kaliteUrl, wikiUrl;
+var port = process.env.NODE_PORT || 3000;
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    host = null;
+	kaliteUrl = 'http://' + add + ':8008';
+	wikiUrl   = 'http://' + add + ':8080';
+  });
+});
 
 var app = express();
 
